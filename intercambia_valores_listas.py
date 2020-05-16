a8e26b1568c18be5064de0d15bc15cce4558c04e
carlos ladron de guevara <carlosldgm@gmail.com>
2020-05-16 18:19:33 -0400

    intercambia valores arreglo ojo no se puede hacer a=b porque quedan apuntando al mismo objeto

---
+++ intercambia_valores_listas.py
@@ -0,0 +1,13 @@
+a = [1,2,3]
+b = [4,5,6]
+
+#pasa los elementos del arreglo a a b
+print("Valores originales")
+print(f"a {a}")
+print(f"b {b}")
+c = a[:]
+a = b[:]
+b = c[:]
+print("Valores Intercambiados")
+print(f"a {a}")
+print(f"b {b}")
